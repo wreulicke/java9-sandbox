76a5be2153f0593590eeedcf5f61cc83e8e69ee2
wreulicke <saito.masaya@classmethod.jp>
2017-10-09 02:47:07 +0900

    add gradle/maven example

---
+++ practice9/src/test/java/com/github/wreulicke/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.github.wreulicke;
+
+import org.junit.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void test(){
+        System.out.println("test");
+        new HelloWorld();
+    }
+}
